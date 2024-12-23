22b57ab18ef909a4409451ab0a12322b93ffcec6
Jan Gustafsson <jannegpriv@gmail.com>
2024-12-23 21:49:51 +0100

    Update ESLint dependencies and configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,35 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
+    node: true
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off'
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 }
