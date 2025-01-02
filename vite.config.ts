bd0a828458b7e78cf337c2f934fd7dfdfc113ec7
Jan Gustafsson <jannegpriv@gmail.com>
2025-01-02 20:08:41 +0100

    feat: update CI/CD to use git tags for versioning

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,18 @@ export default defineConfig({
     watch: {
       usePolling: true
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    },
+    manifest: true,
+    sourcemap: false
   }
 })
